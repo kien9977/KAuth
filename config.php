0ef0bc6fd937d7919e08060c6c6e1f000f3cafb2
airforceonecoder <kiennguyendeptrai1999@gmail.com>
2022-04-19 14:51:25 +0700

    reformat config.php

--- config.php
+++ config.php
@@ -1,5 +1,6 @@
 <?php
 	/*
+	--------------------------------------------------------
 		This section is for SQL configuration
 	*/
 	$_SQLINFO['server'] = 'localhost';
@@ -10,15 +11,38 @@
 
 	/*
 		End of SQL configuration
+	--------------------------------------------------------
 	*/
 
 
 	/*
+	--------------------------------------------------------
 		This section is for app configuration
 	*/
 	$_APP['admin_secret'] = '123123';
 	$_APP['debug'] = true;
 	/*
 		End of app configuration
+	--------------------------------------------------------
 	*/
+
+	/*
+	--------------------------------------------------------
+		Init an SQL connection and secure it
+	*/
+
+	// connect to database
+	$conn = mysqli_connect($host,$username,$password,$database) or die('Cannot connect to database');	
+
+	// To avoid SQLi it should define
+	mysqli_query($conn, "SET NAMES 'utf8mb4'");
+	mysqli_query($conn, "SET CHARACTER SET utf8mb4");
+	mysqli_query($conn, "SET SESSION collation_connection = 'utf32_general_ci'");
+	
+	/* 
+		End of SQL init
+	--------------------------------------------------------
+	*/
+
+	
 ?>
